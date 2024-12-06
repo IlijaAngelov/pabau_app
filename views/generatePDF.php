e33a019fc3f2cb378e90bfb991f2d6608b35e7e2
IlijaAngelov <ilija.angelov@yahoo.com>
2024-12-06 07:04:13 +0100

    Added Test Certificate

---
+++ views/generatePDF.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../config/fpdf186/fpdf.php';
+
+error_reporting(E_ALL); ini_set('display_errors',1);
+
+
+class generatePDF extends FPDF
+{
+    function Header(): void
+    {
+        $this->Image('cert.png',40,60,150, 150);
+        $this->setFont('Arial','B',16);
+        $this->Cell(80);
+        $this->Cell(30, 10, 'CERTIFICATE', 0, 0, 'C');
+        $this->Ln(20);
+    }
+
+    function Footer(): void
+    {
+        $this->SetY(-15);
+        $this->setFont('Arial','I',8);
+        $this->Cell(0,10, 'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+}
+
+$pdf = new generatePDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+$pdf->MultiCell(210,200,"Congratulations, Name Last",0,'C');
+$pdf->Output();
--- views/index.php
+++ views/index.php
@@ -4,3 +4,15 @@
     <a href="categoryWinners.php" type="button" class="btn btn-secondary btn-lg">Check the Winners!</a>
 </div>
 </body>
+
+
+1. DONE - NAPRAJ who voted the most,
+2. DONE - poleto da e mandatory( kontorlero ),
+
+3. DONE - podobar CSS za formata i winners page.
+4. DONE - da nemozes sam da se predlozes
+5. DONE - get winners for each category
+
++ dodadi schema for DB table creation
++ fix routes
++ As a bonus feature, include functionality to generate certificates for the winners of each category.
