c163384468b85b507d6007a6817997711abfc706
IlijaAngelov <ilija.angelov@yahoo.com>
2024-12-07 10:28:37 +0100

    Remove comments

--- views/index.php
+++ views/index.php
@@ -6,13 +6,3 @@
 </body>
 
 
-1. DONE - NAPRAJ who voted the most,
-2. DONE - poleto da e mandatory( kontorlero ),
-
-3. DONE - podobar CSS za formata i winners page.
-4. DONE - da nemozes sam da se predlozes
-5. DONE - get winners for each category
-
-+ dodadi schema for DB table creation
-+ fix routes
-+ As a bonus feature, include functionality to generate certificates for the winners of each category.
